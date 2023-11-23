bec6fb5bc704d5a799dc6df3eda197e242e2b041
Noiseneks <somebodymaybe7134@gmail.com>
2023-11-23 07:21:02 +0300

    Readme + Workflow fix + Renaming

--- src/main/java/com/github/Noiseneks/SpringTest.java
+++ src/main/java/com/github/Noiseneks/SpringTest.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class SpringTest {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main.class);
+        SpringApplication app = new SpringApplication(SpringTest.class);
         app.run(args);
     }
 
